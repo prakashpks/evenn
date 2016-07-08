76fab6fe576fdd12f333281ff1fc4525c2795c80
prakashpks <prakars878740@gmail.com>
2016-07-08 16:27:28 +0530

    finish

---
+++ README.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class number
+{
+	public static void main (String[] args){ 
+	 	System.out.println("Enter number");
+	  Scanner in =new Scanner(System.in);
+	  Integer number,num;
+	  number=in.nextInt();
+	 for(num=0;num<=number;num++)
+		 if(num%2==0)
+			
+	 	System.out.println(num);
+	}
+}
